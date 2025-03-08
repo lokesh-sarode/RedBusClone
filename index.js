495d23fd685816c2612948c4b23a53b88a9f0748
DarkVenomLS <sarodelokesh005@gmail.com>
2025-03-08 16:44:27 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// const searchBtn = document.getElementById("search-button")
+
+// searchBtn.addEventListener('click', function ()
+// {
+//     const source = document.getElementById('source')
+//     const destination = document.getElementById('destination')
+//     const date = document.getElementById('date')
+
+//     const queryParams = new URLSearchParams(
+//         {
+//             source: source,
+//             destination: destination,
+//             date: date,
+//         }
+//     )
+//     window.location.href = `search-result.html?${queryParams.toString()}`
+// })
+
+
+const searchBtn = document.getElementById("search-button");
+
+searchBtn.addEventListener('click', function () {
+    const source = document.getElementById('source').value; // Get the value of the source input
+    const destination = document.getElementById('destination').value; // Get the value of the destination input
+    const date = document.getElementById('date').value; // Get the value of the date input
+
+    // Construct the URL with query parameters
+    const url = `search-result.html?source=${encodeURIComponent(source)}&destination=${encodeURIComponent(destination)}&date=${encodeURIComponent(date)}`;
+
+    // Redirect to the search-result.html page with the query parameters
+    window.location.href = url;
+});
